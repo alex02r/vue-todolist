bdc69421b74775073d2998111dfb166f2db3ed35
alex02r <132152331+alex02r@users.noreply.github.com>
2023-12-13 14:15:12 +0100

    [UP]Aggiunto l'array di ogetti per la todolist

--- js/script.js
+++ js/script.js
@@ -3,7 +3,24 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            
+            todo_list :[
+                {
+                    text : 'Fare la spesa',
+                    done : false
+                },
+                {
+                    text : 'Fare i compiti',
+                    done : false
+                },
+                {
+                    text : 'Pulire la macchina',
+                    done : false
+                },
+                {
+                    text : 'Aggiustare il computer',
+                    done : false
+                }
+            ] 
         }
     },
     methods : {
